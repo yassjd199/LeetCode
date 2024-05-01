bcc3e0acd567ddc83dab3e2b92b14d5aefde29a0
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2024-05-01 13:56:44 +0100

    Time: 3 ms (63.14%), Space: 20.8 MB (7.00%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.cpp
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -1,28 +1,23 @@
 class Solution {
-public:
-    bool verifSeq(string seq){
-        
-        
-        return 1;
-    }
-    vector<string> v;
-    void gen(int i,int n,string res,int sum){
-        //cout<<res<<" ";
-        if(sum<0) return ;
-        if(i>=2*n){
-            //cout<<sum<<" "<<res<<endl;
-            if(sum==0) {
-                
-                v.emplace_back(res) ;
+    public:
+        vector<string> v;
+        void gen(int i, int n, string res, int sum) {
+            //cout<<res<<" ";
+            if (sum < 0) return ;
+            if (i >= 2 * n) {
+                //cout<<sum<<" "<<res<<endl;
+                if (sum == 0) {
+
+                    v.emplace_back(res) ;
+                }
+                return ;
             }
-            return ;
+            gen(i + 1, n, res + "(", sum + 1);
+            gen(i + 1, n, res + ")", sum - 1);
         }
-        gen(i+1,n,res+"(",sum+1);
-        gen(i+1,n,res+")",sum-1);
-    }
-    vector<string> generateParenthesis(int n) {
-        gen(0,n,"",0);
-        return v;
-        
-    }
+        vector<string> generateParenthesis(int n) {
+            gen(0, n, "", 0);
+            return v;
+
+        }
 };
