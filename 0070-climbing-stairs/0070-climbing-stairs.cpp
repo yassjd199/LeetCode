cb4822e4af015ef370ca180229e2ff707621bfcf
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2024-05-03 23:53:54 +0100

    Time: 3 ms (28.70%), Space: 7.3 MB (50.29%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -1,22 +1,20 @@
 class Solution {
-public:
-    
-    int Ways(int i, vector<int> &dp){
-        // base case
-        if(i<=0){
-            return i==0;
+    public:
+
+        int Ways(int i, vector<int> &dp) {
+            // base case
+            if (i <= 0) {
+                return i == 0;
+            }
+            if (dp[i] != -1) return dp[i];
+            int res = Ways(i - 1, dp) + Ways(i - 2, dp);
+            return dp[i] = res;
+
         }
-        if(dp[i]!=-1) return dp[i];
-        int res= Ways(i-1,dp)+Ways(i-2,dp);
-        return dp[i]=res;
-        
-    }
-    
-    
-    
-    int climbStairs(int n) {
-        vector<int> dp(n+1,-1);
-        return Ways(n,dp);
-        
-    }
+
+        int climbStairs(int n) {
+            vector<int> dp(n + 1, -1);
+            return Ways(n, dp);
+
+        }
 };
