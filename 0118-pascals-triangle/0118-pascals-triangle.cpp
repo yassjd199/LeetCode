9ca5ecca4c1a9676e32eca0e9020a4f8d7e23799
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2024-05-05 22:24:53 +0100

    Time: 0 ms (100.00%), Space: 8 MB (18.01%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.cpp
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -1,18 +1,18 @@
 class Solution {
-public:
-    vector<vector<int>> generate(int numRows) {
-        vector<vector<int>> res;
-        res.push_back({1});
-        for(int i=1;i<numRows;i++){
-            vector<int> tmp;
-            tmp.emplace_back(1);
-            for(int j=1;j<res.back().size();j++){
-                tmp.emplace_back(res.back()[j]+res.back()[j-1]);
+    public:
+        vector<vector<int>> generate(int numRows) {
+            vector<vector<int>> res;
+            res.push_back({1});
+            for (int i = 1; i < numRows; i++) {
+                vector<int> tmp;
+                tmp.emplace_back(1);
+                for (int j = 1; j < res.back().size(); j++) {
+                    tmp.emplace_back(res.back()[j] + res.back()[j - 1]);
+                }
+                tmp.emplace_back(1);
+                res.emplace_back(tmp);
             }
-            tmp.emplace_back(1);
-            res.emplace_back(tmp);
+            return res;
+
         }
-        return res;
-        
-    }
 };
