74faf4ec212db4cf75ebfe1e044c945b604c5373
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2024-05-05 11:53:44 +0100

    Time: 6 ms (33.44%), Space: 18.3 MB (6.25%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,23 +1,23 @@
 class Solution {
-public:
-    bool verif(int i,int sz,vector<string>& strs){
-        for(auto str : strs){
-            if(str[i]!=strs[0][i]) return 0;
+    public:
+        bool verif(int i, int sz, vector<string>& strs) {
+            for (auto str : strs) {
+                if (str[i] != strs[0][i]) return 0;
+            }
+            return 1;
+
         }
-        return 1;
-        
-    }
-    string longestCommonPrefix(vector<string>& strs) {
-        int sz=250;
-        for(auto str : strs) {
-            sz=min(sz,(int)str.size());
+        string longestCommonPrefix(vector<string>& strs) {
+            int sz = 250;
+            for (auto str : strs) {
+                sz = min(sz, (int)str.size());
+            }
+            int longestCommonPrefixSz = 0;
+            for (int i = 0; i < sz; i++) {
+                if (verif(i, sz, strs)) longestCommonPrefixSz++;
+                else break ;
+            }
+            return strs[0].substr(0, longestCommonPrefixSz);
+
         }
-        int longestCommonPrefixSz=0;
-        for(int i=0;i<sz;i++) {
-            if(verif(i,sz,strs)) longestCommonPrefixSz++;
-            else break ;
-        }
-        return strs[0].substr(0,longestCommonPrefixSz);
-        
-    }
 };
