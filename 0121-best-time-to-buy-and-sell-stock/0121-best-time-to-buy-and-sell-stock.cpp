5c0cb052076c2779af55f17eeccf4c6459eb17b0
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2024-05-05 01:28:48 +0100

    Time: 247 ms (5.40%), Space: 165.5 MB (5.53%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,39 +1,20 @@
-struct SparseTable {
-    int n, m;
-    vector<vector<int>> sp;
-    int merg(int l, int r) {
-        // change operation here ;
-        return max(l, r);
-    }
-    SparseTable(int _n) {
-        n = _n;
-        m = (int)log2(n) + 2;
-        sp.resize(n, vector<int>(m));
-    }
-    void build(vector<int> &v) {
-        for (int i = 0; i < n; i++) sp[i][0] = v[i];
-        for (int j = 1; j < m; j++)
-            for (int i = 0; i + (1 << j) <= n; i++)
-                sp[i][j] = merg(sp[i][j - 1], sp[i + (1 << (j - 1))][j - 1]);
-    }
-    int query(int l, int r) {
-        if (l > n) return 0;
-        if (r - l + 1 == 0) return 0;
-        int x = (int)log2(r - l + 1);
-        return merg(sp[l][x], sp[r - (1 << x) + 1][x]);
-    }
-};
-
 class Solution {
     public:
-        int maxProfit(vector<int>& prices) {
-            SparseTable sp((int)prices.size());
-            sp.build(prices);
-            int res = 0;
-            for (int i = 0; i < (int)prices.size(); i++) {
-                res = max(res, sp.query(i + 1, (int)prices.size() - 1) - prices[i]);
-            }
+        int getAnswer(int i, int prev, vector<int> &prices, vector<vector<int>> &dp) {
+            if (prev == 2) return 0;
+            if (i >= (int)prices.size()) return 0;
+
+            int &res = dp[i][prev];
+            if (~res) return res;
+            res = getAnswer(i + 1, prev, prices, dp);
+            if (prev == 0) res = max(res, getAnswer(i + 1, 1, prices, dp) - prices[i]);
+            if (prev == 1) res = max(res, getAnswer(i + 1, 2, prices, dp) + prices[i]);
             return res;
+
+        }
+        int maxProfit(vector<int>& prices) {
+            vector<vector<int>> dp(prices.size() + 1, vector<int>(3, -1));
+            return getAnswer(0, 0, prices, dp);
 
         }
 };
