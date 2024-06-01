678b521480e440e7b04851c73287e4b77db6acbe
Yassine Jedai <110100590+yassjd199@users.noreply.github.com>
2024-06-01 23:14:46 +0100

    Time: 55 ms (69.04%), Space: 45.4 MB (47.37%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int n=people.size();
+        int l=0,r=n-1;
+        int ans=0;
+        while(l<=r){
+            if(l==r){
+                ans++;
+                return ans;
+            }
+            if(people[l]+people[r]<=limit) ans++,l++,r--;
+            else ans++,r--;
+        }
+        return ans;
+        
+    }
+};
